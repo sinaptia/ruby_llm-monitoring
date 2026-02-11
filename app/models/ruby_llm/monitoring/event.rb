49a55d201e7fca557381b7305592232617828ba9
Michael Moen <mmoen@vivoaquatics.com>
2026-02-11 07:54:31 -0600

    simplify logic per feedback

--- app/models/ruby_llm/monitoring/event.rb
+++ app/models/ruby_llm/monitoring/event.rb
@@ -11,11 +11,9 @@ module RubyLLM::Monitoring
 
       self.cost = if provider.local? || [ payload["input_tokens"], payload["output_tokens"] ].all?(nil)
         0.0
-      elsif model.modalities.output == ["embeddings"]
-        payload["input_tokens"] / 1_000_000.0 * model.input_price_per_million
       else
-        input_cost = payload["input_tokens"] / 1_000_000.0 * model.input_price_per_million
-        output_cost = payload["output_tokens"] / 1_000_000.0 * model.output_price_per_million
+        input_cost = payload["input_tokens"].to_f / 1_000_000.0 * model.input_price_per_million.to_f
+        output_cost = payload["output_tokens"].to_f / 1_000_000.0 * model.output_price_per_million.to_f
 
         input_cost + output_cost
       end
